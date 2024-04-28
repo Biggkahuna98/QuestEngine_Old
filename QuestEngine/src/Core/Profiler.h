5e0f29ff4836874ee9ff113930742c7ebafc0619
Alex Sederquest <alexsederquest98@gmail.com>
2024-04-28 00:25:12 -0400

    Added profiler header for Tracy macros

---
+++ QuestEngine/src/Core/Profiler.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define QE_ENABLE_PROFILING QUEST_DEBUG_MODE
+
+#if QE_ENABLE_PROFILING
+#include <tracy/Tracy.hpp>
+
+/*
+QE_PROFILE_FRAME goes at the end of the frame loop
+QE_PROFILE_FUNC(name) or whatever you want to call it, goes at the top of a function you want profiled
+
+*/
+#define QE_PROFILE_FRAME		FrameMark
+#define QE_PROFILE_FUNC(name)	ZoneScopedN(name)
+#define QE_PROFILE_SCOPE		ZoneScoped
+
+#else
+
+#define QE_PROFILE_FRAME
+#define QE_PROFILE_FUNC(name)
+#define QE_PROFILE_SCOPE
+
+#endif
